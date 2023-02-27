b4ed050ee2a26b0682a91e6cf33a80fc6bbeda3c
BushraAbdullahi <bushra1175@gmail.com>
2023-02-27 16:57:05 +0000

    returns cabinet list using beautifulsoup

--- api/api.py
+++ api/api.py
@@ -4,8 +4,8 @@ from flask import Flask
 from flask_cors import CORS, cross_origin
 from flask import jsonify
 from bs4 import BeautifulSoup
-from flask import Flask, render_template
-from urllib.request import urlopen
+from flask import app, make_response, render_template
+import urllib.request as urllib
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -15,21 +15,12 @@ def getQuery(search):
     results = response.json()
     return jsonify(results)
 
-# pmPageID = 24150
-# HomeSecPageID = 149104
-# query = 'https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&pageids={}'
-# @app.route('/')
-# @cross_origin()
-# def index():
-#     result = getQuery(query.format(HomeSecPageID))
-#     return result 
-
-url = 'https://www.gov.uk/government/ministers'
-soup = BeautifulSoup(requests.get(url).text,'html.parser')
 
 @app.route('/')
 @cross_origin()
 def index():
-    html = urlopen('https://www.gov.uk/government/ministers')
-    bs = getQuery(BeautifulSoup(html, 'html.parser'))
-    return bs
+    url = 'https://www.gov.uk/government/ministers'
+    page = urllib.urlopen(url)
+    soup = BeautifulSoup(page, 'html.parser')
+    cabinet_list = soup.find('ul', attrs={'class': 'cabinet-list'})
+    return str(cabinet_list)
