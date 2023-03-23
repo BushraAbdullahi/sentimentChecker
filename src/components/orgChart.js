6cb5f685447d081ccc6baaae44eff82f218a136b
BushraAbdullahi <bushra1175@gmail.com>
2023-03-23 23:44:36 +0000

    completed pie chart

--- src/components/orgChart.js
+++ src/components/orgChart.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { PieChart } from 'react-minimal-pie-chart'
 
 // Define a component named OrgChart.
 const OrgChart = () => {
-  // // Declare state variables.
+  // Declare state variables.
   const [ministers, setMinisters] = useState({})
 
   useEffect(() => {
@@ -14,7 +15,7 @@ const OrgChart = () => {
       .catch((err) => console.error(err))
   }, [])
 
-  // // Declare a state variable.
+  // Declare a state variable.
   const [sentiments, setSentiments] = useState({})
 
   useEffect(() => {
@@ -27,32 +28,66 @@ const OrgChart = () => {
   function getSentimentValue(key, sentiment) {
     if (sentiments[key] && sentiments[key][sentiment]) {
       return sentiments[key][sentiment]
+    } else {
+      return 0
     }
-    return null
   }
 
   return (
     <div className="org-chart">
-      {Object.entries(ministers).map(([key, value], index) => (
-        <div className="card" key={key}>
-          <div className="circle">
-            <img src={value.img_src} alt={value.name} />
+      {Object.entries(ministers).map(([key, value], index) => {
+        const positive_percentage = getSentimentValue(
+          value.name,
+          'positive_percentage',
+        )
+        const neutral_percentage = getSentimentValue(
+          value.name,
+          'neutral_percentage',
+        )
+        const negative_percentage = getSentimentValue(
+          value.name,
+          'negative_percentage',
+        )
+        const hasData =
+          positive_percentage > 0 ||
+          neutral_percentage > 0 ||
+          negative_percentage > 0
+
+        return (
+          <div className="card" key={key}>
+            <div className="circle">
+              <img src={value.img_src} alt={value.name} />
+            </div>
+            <h2>{value.name}</h2>
+            <h3>{value.role}</h3>
+            <div className="pie-chart">
+              {hasData ? (
+                <PieChart
+                  data={[
+                    {
+                      title: 'Positive Percentage: ' + positive_percentage,
+                      value: positive_percentage,
+                      color: '#ABFF77',
+                    },
+                    {
+                      title: 'Neutral Percentage: ' + neutral_percentage,
+                      value: neutral_percentage,
+                      color: '#FFED78',
+                    },
+                    {
+                      title: 'Negative Percentage: ' + negative_percentage,
+                      value: negative_percentage,
+                      color: '#FF7878',
+                    },
+                  ]}
+                />
+              ) : (
+                <div>No Data</div>
+              )}
+            </div>
           </div>
-          <h2>{value.name}</h2>
-          <h3>{value.role}</h3>
-          <div className="progress" style={{ maxWidth: '50%' }}>
-            <div class="progress-bar bg-success" style={{ width: String(getSentimentValue(value.name, 'positive_percentage')) }}>
-            {getSentimentValue(value.name, 'positive_percentage')}
-            </div>
-            <div class="progress-bar bg-warning" style={{ width: String(getSentimentValue(value.name, 'neutral_percentage')) }}>
-            {getSentimentValue(value.name, 'neutral_percentage')}
-            </div>
-            <div class="progress-bar bg-danger" style={{ width: String(getSentimentValue(value.name, 'negative_percentage')) }}>
-            {getSentimentValue(value.name, 'negative_percentage')}
-            </div>
-          </div>
-        </div>
-      ))}
+        )
+      })}
     </div>
   )
 }
