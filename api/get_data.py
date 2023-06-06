4ed39000a3ab2ecbf18915ecb6c56206b736e953
BushraAbdullahi <103956365+BushraAbdullahi@users.noreply.github.com>
2023-06-06 15:53:25 +0100

    added print statements

--- api/get_data.py
+++ api/get_data.py
@@ -10,7 +10,6 @@ load_dotenv()
 
 Base = declarative_base()
 
-
 class CabinetMinister(Base):
     __tablename__ = 'cabinet_ministers'
     id = Column(Integer, primary_key=True)
@@ -18,7 +17,6 @@ class CabinetMinister(Base):
     role = Column(String)
     img_src = Column(String)
     dateTime = Column(String)
-
 
 class Sentiment(Base):
     __tablename__ = 'sentiments'
@@ -29,24 +27,20 @@ class Sentiment(Base):
     neutral_score = Column(Numeric)
     dateTime = Column(String)
 
-
 engine = create_engine(os.getenv('DATABASE_URL'))
 Session = sessionmaker(bind=engine)
-
 
 def tweets(count):
     session = Session()
     all_tweets = {}
-
     for minister in session.query(CabinetMinister).all():
         tweets = methods.get_tweets(minister.name, tweet_count=count)
         all_tweets[minister.name] = tweets
+    print(f'Collected {sum([len(tweets) for tweets in all_tweets.values()])} tweets.')
     return all_tweets
-
 
 def clean_tweets(tweets_dict):
     cleaned_dict = {}
-
     for minister, tweets in tweets_dict.items():
         tweets = methods.remove_retweets(tweets)
         tweets = methods.lowercase_tweets(tweets)
@@ -54,74 +48,48 @@ def clean_tweets(tweets_dict):
         tweets = methods.remove_stop_words(tweets)
         tweets = methods.lemmatize_tweets(tweets)
         cleaned_tweets = methods.remove_duplicates(tweets)
-
         cleaned_dict[minister] = cleaned_tweets
-
+    print('Tweets cleaned.')
     return cleaned_dict
-
 
 def scrape_and_store_ministers():
     session = Session()
-
     govPage = 'https://www.gov.uk/government/ministers'
-
-    names = methods.getAttributes(
-        govPage, 'a', 'gem-c-image-card__title-link govuk-link')
-    roles = methods.getAttributes(
-        govPage, 'div', 'gem-c-image-card__description')
+    names = methods.getAttributes(govPage, 'a', 'gem-c-image-card__title-link govuk-link')
+    roles = methods.getAttributes(govPage, 'div', 'gem-c-image-card__description')
     images = methods.getAttributes(govPage, 'img', '')
-
     combined_list = []
     for name, role, image in zip(names, roles, images):
         combined_list.append({
             "name": name,
             "role": role,
             "img_src": image["img_src"],
-            "dateTime": datetime.now().strftime('%d-%m-%Y %H:%M')  # Add the current time
+            "dateTime": datetime.now().strftime('%d-%m-%Y %H:%M')
         })
 
     session.query(CabinetMinister).delete()
     session.commit()
+    print('Old ministers deleted from the database.')
 
     for data in combined_list:
-        minister = CabinetMinister(
-            name=data['name'],
-            role=data['role'],
-            img_src=data['img_src'],
-            dateTime=data['dateTime']  
-        )
+        minister = CabinetMinister(name=data['name'], role=data['role'], img_src=data['img_src'], dateTime=data['dateTime'])
         session.merge(minister)
-
     session.commit()
-
+    print('New ministers scraped and stored in the database.')
 
 def analyse_tweets():
     raw_tweets = tweets(1000)
-
     cleaned_dict = clean_tweets(raw_tweets)
-
     for minister, cleaned_tweets in cleaned_dict.items():
         session = Session()
-
-        session.query(Sentiment).filter(
-            Sentiment.minister == minister).delete()
-
+        session.query(Sentiment).filter(Sentiment.minister == minister).delete()
         sentiment_result = methods.sentiment_checker(cleaned_tweets)
-
-        sentiment = Sentiment(
-            minister=minister,
-            positive_score=sentiment_result['positive_percentage'],
-            negative_score=sentiment_result['negative_percentage'],
-            neutral_score=sentiment_result['neutral_percentage'],
-            dateTime=datetime.now().strftime('%d-%m-%Y %H:%M')
-        )
-
+        sentiment = Sentiment(minister=minister, positive_score=sentiment_result['positive_percentage'], negative_score=sentiment_result['negative_percentage'], neutral_score=sentiment_result['neutral_percentage'], dateTime=datetime.now().strftime('%d-%m-%Y %H:%M'))
         session.add(sentiment)
-
         session.commit()
-
+        print(f'Sentiment analysis completed for {minister}.')
         session.close()
-
 
 scrape_and_store_ministers()
 analyse_tweets()
+print('All tasks completed.')
